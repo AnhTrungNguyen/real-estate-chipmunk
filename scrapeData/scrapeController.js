58166749fa831cb9f30903221e64a9332ab4522d
AnhTrungNguyen <103439199+AnhTrungNguyen@users.noreply.github.com>
2023-08-01 16:08:20 +0700

    Success scraper data and upload base server

--- scrapeData/scrapeController.js
+++ scrapeData/scrapeController.js
@@ -1,13 +1,39 @@
 const scrapers = require('./scraper')
+const fs = require('fs')
 
 const scrapeController = async (browserInstance) => {
     const url = 'https://phongtro123.com/'
-    const indexs = [1,2,3,4]
+    const indexs = [1, 2, 3, 4]
     try {
         let browser = await browserInstance
-        const categories = await scrapers.scrapeCategory(browser,url)
+        const categories = await scrapers.scrapeCategory(browser, url)
         const selectedCategory = categories.filter((category, index) => indexs.some(i => i === index))
-        await scrapers.scraper(browser,selectedCategory[0].link)
+
+        let result1 = await scrapers.scraper(browser, selectedCategory[0].link)
+        fs.writeFile('chothuephongtro.json', JSON.stringify(result1), (err) => {
+            if (err) console.log('Ghi data vô file json thất bại: ' + err)
+            console.log('Ghi data vô file json thành công')
+        })
+
+        let result2 = await scrapers.scraper(browser, selectedCategory[1].link)
+        fs.writeFile('nhachothue.json', JSON.stringify(result2), (err) => {
+            if (err) console.log('Ghi data vô file json thất bại: ' + err)
+            console.log('Ghi data vô file json thành công')
+        })
+
+        let result3 = await scrapers.scraper(browser, selectedCategory[2].link)
+        fs.writeFile('chothuecanho.json', JSON.stringify(result3), (err) => {
+            if (err) console.log('Ghi data vô file json thất bại: ' + err)
+            console.log('Ghi data vô file json thành công')
+        })
+
+        let result4 = await scrapers.scraper(browser, selectedCategory[3].link)
+        fs.writeFile('chothuematbang.json', JSON.stringify(result4), (err) => {
+            if (err) console.log('Ghi data vô file json thất bại: ' + err)
+            console.log('Ghi data vô file json thành công')
+        })
+
+        await browser.close()
 
     } catch (error) {
         console.log('Lỗi ở scrape controller' + error)
--- scrapeData/scraper.js
+++ scrapeData/scraper.js
@@ -1,12 +1,15 @@
-const scrapeCategory = (browser, url) => new Promise(async(resolve,reject) => {
+const scrapeCategory = (browser, url) => new Promise(async (resolve, reject) => {
     try {
         let page = await browser.newPage()
+        console.log('>> Mở tab mới ...');
         await page.goto(url)
+        console.log('>>Truy cập vào ' + url)
         await page.waitForSelector('#webpage')
+        console.log('>> Website đã laod xong...');
 
         const dataCategory = await page.$$eval('#navbar-menu > ul > li', els => {
             dataCategory = els.map(el => {
-                return{
+                return {
                     category: el.querySelector('a').innerText,
                     link: el.querySelector('a').href
                 }
@@ -14,27 +17,30 @@ const scrapeCategory = (browser, url) => new Promise(async(resolve,reject) => {
             return dataCategory
         })
 
-        await browser.close()
+        await page.close()
         resolve(dataCategory)
     } catch (error) {
         console.log('Lỗi ở scrape category' + error)
         reject(error)
     }
-}) 
+})
 
-const scraper = (browser, url) => new Promise(async(resolve,reject) => {
+const scraper = (browser, url) => new Promise(async (resolve, reject) => {
     try {
         let newPage = await browser.newPage()
+        console.log('>> Đã mở tab mới ...');
         await newPage.goto(url)
+        console.log(">> Đã truy cập vào trang " + url)
         await newPage.waitForSelector('#main')
+        console.log('>> Đã load xong tag main ...');
         const scrapeData = {}
         const headerData = await newPage.$eval('header', (el) => {
-                return{
-                    title: el.querySelector('h1').innerText,
-                    description: el.querySelector('p').innerText
-                }
+            return {
+                title: el.querySelector('h1').innerText,
+                description: el.querySelector('p').innerText
+            }
         })
-              
+
         scrapeData.header = headerData
 
         const detailLinks = await newPage.$$eval('#left-col > section.section-post-listing > ul > li', (els) => {
@@ -44,26 +50,26 @@ const scraper = (browser, url) => new Promise(async(resolve,reject) => {
             return detailLinks
         })
 
-        const scraperDetail = async (link) => new Promise(async(resolve,reject) => {
+        const scraperDetail = async (link) => new Promise(async (resolve, reject) => {
             try {
                 let pageDetail = await browser.newPage()
                 await pageDetail.goto(link)
                 await pageDetail.waitForSelector('#main')
 
                 const detailData = {}
-                const images = await pageDetail.$$eval('#left-col > article > div.post-images > div> div.swiper-wrapper > div.swiper-slide',(els) => {
+                const images = await pageDetail.$$eval('#left-col > article > div.post-images > div> div.swiper-wrapper > div.swiper-slide', (els) => {
                     images = els.map(el => {
-                        return el.querySelector('img').src
+                        return el.querySelector('img')?.src
                     })
-                    return images
+                    return images.filter(i => !i === false)
                 })
 
                 detailData.images = images
 
-                const header = await pageDetail.$eval('header.page-header',(el) => {
+                const header = await pageDetail.$eval('header.page-header', (el) => {
                     return {
                         title: el.querySelector('h1 > a').innerText,
-                        star: ql.querySelector('h1 > span').className.replace(/^\D+/g,''),
+                        star: el.querySelector('h1 > span')?.className?.replace(/^\D+/g, ''),
                         class: {
                             content: el.querySelector('p').innerText,
                             classType: el.querySelector('p > a > strong').innerText
@@ -78,25 +84,67 @@ const scraper = (browser, url) => new Promise(async(resolve,reject) => {
                     }
                 })
 
+                detailData.header = header
+                // thông tin mô tả
+                const mainContentHeader = await pageDetail
+                    .$eval('#left-col > article.the-post > section.post-main-content', (el) => el.querySelector('div.section-header > h2').innerText)
+                const mainContentContent = await pageDetail
+                    .$$eval('#left-col > article.the-post > section.post-main-content > .section-content > p', (els) => els.map(el => el.innerText))
+                detailData.mainContent = {
+                    header: mainContentHeader,
+                    content: mainContentContent
+                }
+
+                // đặc điểm tin đăng
+                const overviewHeader = await pageDetail
+                    .$eval('#left-col > article.the-post > section.post-overview', (el) => el.querySelector('div.section-header > h3').innerText)
+                const overviewContent = await pageDetail
+                    .$$eval('#left-col > article.the-post > section.post-overview > .section-content > table.table > tbody > tr', (els) => els.map(el => ({
+                        name: el.querySelector('td:first-child').innerText,
+                        content: el.querySelector('td:last-child').innerText
+                    })))
+                detailData.overview = {
+                    header: overviewHeader,
+                    content: overviewContent
+                }
+
+                // thông tin liên hệ
+                const contactHeader = await pageDetail
+                    .$eval('#left-col > article.the-post > section.post-contact', (el) => el.querySelector('div.section-header > h3').innerText)
+                const contactContent = await pageDetail
+                    .$$eval('#left-col > article.the-post > section.post-contact > .section-content > table.table > tbody > tr', (els) => els.map(el => ({
+                        name: el.querySelector('td:first-child').innerText,
+                        content: el.querySelector('td:last-child').innerText
+                    })))
+
+                detailData.contact = {
+                    header: contactHeader,
+                    content: contactContent
+                }
+
                 await pageDetail.close()
-                resolve()
+                console.log('>> Đã đóng tab ' + link)
+                resolve(detailData)
             } catch (error) {
                 console.log('Lấy data detail lỗi' + error)
                 reject(error)
             }
         })
 
+        const details = []
         for (let link of detailLinks) {
-           await scraperDetail(link)
+            const detail = await scraperDetail(link)
+            details.push(detail)
         }
+        scrapeData.body = details
 
-        await browser.close()
-        resolve()
+        console.log('>> Trình duyệt đã đóng.');
+        resolve(scrapeData)
     } catch (error) {
         console.log('Lỗi ở scraper' + error)
         reject(error)
     }
-}) 
+})
 
 module.exports = {
     scrapeCategory,
