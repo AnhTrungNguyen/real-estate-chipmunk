eb569cde5485f9a3af2b9119dadf0b5b7dc4d6b1
AnhTrungNguyen <103439199+AnhTrungNguyen@users.noreply.github.com>
2023-08-03 09:07:08 +0700

    Scraper data in database

--- client/src/components/InputForm.js
+++ client/src/components/InputForm.js
@@ -1,12 +1,20 @@
-import React, {memo} from 'react'
+import React, { memo } from 'react'
 
-const inputForm = ({label}) => {
+const InputForm = ({ label, value, setValue, keyPayload, invalidFields, setInvalidFields, type }) => {
   return (
     <div>
-        <label className='text-xs' htmlFor='phone'>{label}</label>
-        <input type='text' id='phone' className='outline-none bg-[#e8f0fe] p-2 rounede-md w-full' />
+      <label htmlFor={keyPayload} className='text-xs' >{label}</label>
+      <input
+        type={type || 'text'}
+        id={keyPayload}
+        className='outline-none bg-[#e8f0fe] p-2 rounded-md w-full'
+        value={value}
+        onChange={(e) => setValue(prev => ({ ...prev, [keyPayload]: e.target.value }))}
+        onFocus={() => setInvalidFields([])}
+      />
+      {invalidFields.length > 0 && invalidFields.some(i => i.name === keyPayload) && <small className='text-red-500 italic' >{invalidFields.find(i => i.name === keyPayload)?.message}</small>}
     </div>
   )
 }
 
-export default memo(inputForm)
+export default memo(InputForm)
