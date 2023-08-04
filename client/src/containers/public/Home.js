2a88d9f0c8a420ebb19f95de445fe92c7e449cd8
AnhTrungNguyen <103439199+AnhTrungNguyen@users.noreply.github.com>
2023-08-04 09:23:16 +0700

    Hoàn thiện UI Homepage

--- client/src/containers/public/Home.js
+++ client/src/containers/public/Home.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
-import Navigation from './Navigation'
+import { Navigation, Search } from './index'
+import { Intro, Contact } from '../../components'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 const Home = () => {
+  const { isLoggedIn } = useSelector(state => state.auth)
+
   return (
-    <div className='w-full flex flex-col items-center h-full m-auto h-full'>
-        <Header />
-        <Navigation />
-        <div className='w-1100 flex flex-col items-center justify-start'>
-          <Outlet />
-        </div>
+    <div className='w-full flex gap-6 flex-col items-center h-full'>
+      <Header />
+      <Navigation />
+      {isLoggedIn && <Search />}
+      <div className='w-4/5 lg:w-3/5 flex flex-col items-start justify-start mt-3'>
+        <Outlet />
+      </div>
+      <Intro />
+      <Contact />
+      <div className='h-[500px]'>
+
+      </div>
     </div>
   )
 }
